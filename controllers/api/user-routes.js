d6a0cf8e52c5623fee14fdf70b9a417be2c35e6e
rmob <riley.obryan@gmail.com>
2023-02-21 06:15:48 -0800

    past entries appear on homepage

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -65,7 +65,7 @@ router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.render('logout')
-      res.status(204).end();
+      // res.status(204).end();
     });
   } else {
     res.status(404).end();
--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    const response = await fetch('/api/user/login', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
@@ -29,7 +29,7 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector('#password-signup').value.trim();
 
   if (username && email && password) {
-    const response = await fetch('/api/user/', {
+    const response = await fetch('/api/users/', {
       method: 'POST',
       body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
