0088efe61a22c1bdef627da7a8a93772b18a11c9
rmob <riley.obryan@gmail.com>
2023-02-25 05:38:20 -0800

    dashboard.hbs restricted to loggedIn user

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -1,5 +1,25 @@
 const router = require("express").Router();
-const { Entry, Comment, User } = require("../models");
-const withAuth = require('../utils/auth');
+const { Entry, Comment, User } = require("../../models");
+const withAuth = require('../../utils/auth');
+
+// GET all entries for homepage
+router.get("/", async (req, res) => {
+    try {
+      const dbEntryData = await Entry.findAll({
+          include: [{ model: User, Comment }]
+      });
+      
+      const entries = dbEntryData.map((entry) => entry.get({ plain: true }));
+      console.log(entries);
+  
+      res.render("dashboard", {
+        entries,
+        loggedIn: req.session.loggedIn,
+      });
+    } catch (err) {
+      console.log(err, "THIS IS ERROR MESSAGE");
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router
