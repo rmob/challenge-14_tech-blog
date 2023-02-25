2a989baaa3d3402c7503b632bf840350615cdee0
rmob <riley.obryan@gmail.com>
2023-02-25 06:26:56 -0800

    create-entry.hbs connected

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -7,6 +7,7 @@ router.get("/", async (req, res) => {
     try {
       const dbEntryData = await Entry.findAll({
           include: [{ model: User, Comment }]
+        //   where: user_id: req.params.id
       });
       
       const entries = dbEntryData.map((entry) => entry.get({ plain: true }));
@@ -21,5 +22,27 @@ router.get("/", async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+router.get("/create", (req, res) => {
+    res.render('create-entry')
+})
+
+router.post("/create", async (req, res) => {
+        try {
+            const dbEntryData = await Entry.create({
+              title: req.body.title,
+              content: req.body.content,
+            });
+
+            res.render("dashboard", {
+                entries,
+                loggedIn: req.session.loggedIn,
+              });
+            } catch (err) {
+              console.log(err, "THIS IS ERROR MESSAGE");
+              res.status(500).json(err);
+            }
+    }
+)
 
 module.exports = router
