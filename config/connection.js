ca97d4e4ea0b82e732eb28f34f298cb824895255
rmob <riley.obryan@gmail.com>
2023-03-20 12:10:48 -0700

    jawdb config

--- config/connection.js
+++ config/connection.js
@@ -15,12 +15,16 @@ require("dotenv").config();
 // module.exports = sequelize;
 
 const sequelize = process.env.JAWSDB_URL
-	? new Sequelize(process.env.JAWSDB_URL)
-	: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
-    {
-			host: process.env.DB_HOST,
-			dialect: 'mysql',
-			port: 3001,
-	  });
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        port: 3001,
+      }
+    );
 
 module.exports = sequelize;
